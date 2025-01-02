b55281f9aff0cdc32bb304d43bb66161e825175e
Pirushov Maxim <maximalka111@yandex.ru>
2025-01-02 20:46:22 +0400

    added url

--- client/src/shared/config/index.ts
+++ client/src/shared/config/index.ts
@@ -1,2 +1,2 @@
-//export const API_URL= 'https://pseudotube-server.vercel.app/api'
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://pseudo-tube-xi87-92xshom8d-maxims-projects-47a50857.vercel.app/api';
+//export const API_URL = 'http://localhost:5000/api';
